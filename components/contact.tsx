73be12545c753c805af76b16a4a3051fe80d9330
ByteCrak07 <abilsavio@gmail.com>
2023-07-17 19:22:45 +0530

    Basic Form UI

---
+++ components/contact.tsx
@@ -0,0 +1,73 @@
+'use client';
+
+import { FC } from 'react';
+import { useForm } from 'react-hook-form';
+import { sendEmail } from '@/utils/send-email';
+
+export type FormData = {
+  name: string;
+  email: string;
+  message: string;
+};
+
+const Contact: FC = () => {
+  const { register, handleSubmit } = useForm<FormData>();
+
+  function onSubmit(data: FormData) {
+    sendEmail(data);
+  }
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <div className='mb-5'>
+        <label
+          htmlFor='name'
+          className='mb-3 block text-base font-medium text-black'
+        >
+          Full Name
+        </label>
+        <input
+          type='text'
+          placeholder='Full Name'
+          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
+          {...register('name', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>
+        <label
+          htmlFor='email'
+          className='mb-3 block text-base font-medium text-black'
+        >
+          Email Address
+        </label>
+        <input
+          type='email'
+          placeholder='example@domain.com'
+          className='w-full rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
+          {...register('email', { required: true })}
+        />
+      </div>
+      <div className='mb-5'>
+        <label
+          htmlFor='message'
+          className='mb-3 block text-base font-medium text-black'
+        >
+          Message
+        </label>
+        <textarea
+          rows={4}
+          placeholder='Type your message'
+          className='w-full resize-none rounded-md border border-gray-300 bg-white py-3 px-6 text-base font-medium text-gray-700 outline-none focus:border-purple-500 focus:shadow-md'
+          {...register('message', { required: true })}
+        ></textarea>
+      </div>
+      <div>
+        <button className='hover:shadow-form rounded-md bg-purple-500 py-3 px-8 text-base font-semibold text-white outline-none'>
+          Submit
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default Contact;
---
+++ utils/send-email.ts
@@ -0,0 +1,6 @@
+import { FormData } from '@/components/contact';
+
+export function sendEmail(data: FormData) {
+  // TODO: send email
+  console.log(data);
+}
