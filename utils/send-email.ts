aeeba1da83e6485242fa9d3fcc481fc5e0d68fbb
ByteCrak07 <abilsavio@gmail.com>
2023-07-18 15:46:39 +0530

    Complete form

--- utils/send-email.ts
+++ utils/send-email.ts
@@ -1,6 +1,17 @@
 import { FormData } from '@/components/contact';
 
 export function sendEmail(data: FormData) {
-  // TODO: send email
-  console.log(data);
+  const apiEndpoint = '/api/email';
+
+  fetch(apiEndpoint, {
+    method: 'POST',
+    body: JSON.stringify(data),
+  })
+    .then((res) => res.json())
+    .then((response) => {
+      alert(response.message);
+    })
+    .catch((err) => {
+      alert(err);
+    });
 }
